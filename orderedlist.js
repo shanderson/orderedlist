7df3fd4b4b821b617f7200627e75cf6b33a46932
Shane Anderson <shaneanderson@Shanes-MacBook-Pro.local>
2021-03-11 12:48:16 -0500

    working with button reordering

--- orderedlist.js
+++ orderedlist.js
@@ -1,8 +1,24 @@
 angular.module('orderedlistExample', [])
 .controller('Controller', ['$scope', '$timeout', function($scope, $timeout) {
-
+  $scope.yourName = "Fred Flintstone";
+  $scope.names = ["Gunter", "George"];
+  $scope.addName = function() {
+    $scope.names.push($scope.yourName);
+  };
+  $scope.moveUpOne = function(index) {
+    if (index <= 0) return;
+    let removed = $scope.names.splice(index, 1);
+    console.log("move up", index, removed[0]);
+    $scope.names.splice(index-1, 0, removed[0]);
+  };
+  $scope.moveDownOne = function(index) {
+    if(index>= $scope.names.length-1) return;
+    let removed = $scope.names.splice(index, 1);
+    console.log("move down", index);
+    $scope.names.splice(index+1, 0, removed[0]);
+  };
 }])
-.directive('list', function() {
+.directive('eoslist', function() {
   return {
     restrict: 'A',
     transclude: true,
